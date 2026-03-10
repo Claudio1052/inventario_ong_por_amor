5c4bbbeddce60cadb3c31c5e6f1b45acea0fa0ad
Claudio1052 <118035028+Claudio1052@users.noreply.github.com>
2026-03-09 21:20:23 -0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,4 @@
+window.APP_CONFIG = {
+  supabaseUrl: 'https://unidzfxifxjruonbjspe.supabase.co',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVuaWR6ZnhpZnhqcnVvbmJqc3BlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI4NTc0MTEsImV4cCI6MjA4ODQzMzQxMX0.0R_FvMXHDvt34bpa5Fi9pkJcu_zGy8Av86wVaXp2F3o'
+};
